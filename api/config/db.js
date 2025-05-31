e94032b66886ab1458b0a2bd194ab03fc61ac255
harshh <bakasakuta33@gmail.com>
2025-05-31 14:31:14 +0530

    database atlass added

--- api/config/db.js
+++ api/config/db.js
@@ -1,17 +1,35 @@
+// const mongoose = require("mongoose");
+
+// const connectWithDB = () => {
+//   mongoose.set("strictQuery", false);
+//   mongoose
+//     .connect(process.env.DB_URL, {
+//       // useNewUrlParser: true,
+//       // useUnifiedTopology: true,
+//     })
+//     .then(console.log(`DB connected successfully`))
+//     .catch((err) => {
+//       console.log(`DB connection failed`);
+//       console.log(err);
+//       process.exit(1);
+//     });
+// };
+
+// module.exports = connectWithDB;
 const mongoose = require("mongoose");
 
 const connectWithDB = () => {
-  mongoose.set("strictQuery", false);
+  mongoose.set("strictQuery", false); // Recommended for Mongoose 6+
   mongoose
     .connect(process.env.DB_URL, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+      // useNewUrlParser: true,    // These options are true by default in Mongoose 6+
+      // useUnifiedTopology: true, // and can usually be omitted.
     })
-    .then(console.log(`DB connected successfully`))
+    .then(() => console.log(`DB connected successfully`)) // Corrected console.log syntax
     .catch((err) => {
       console.log(`DB connection failed`);
       console.log(err);
-      process.exit(1);
+      process.exit(1); // Exit process if connection fails
     });
 };
 
